fe70d3a98cff99dd59b29e8ed5af8947140e48a1
woosungchu <woosungchu@gmail.com>
2017-08-05 16:12:40 +0900

    set Contents

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
     <turtle-header></turtle-header>
+    <!--
     <router-link v-bind:to="'/'">
       <img src="./assets/logo.png">
     </router-link>
-    <router-view></router-view>
+    -->
+
+    <section class="section">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -24,7 +32,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
 }
 </style>
