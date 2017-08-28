56365df834b9a1b0fffe7f1e7e25819306c5b4fc
woosungchu <woosungchu@gmail.com>
2017-08-28 11:23:02 +0900

    prevent access to authLogin page to logged user

--- src/components/common/TurtleAuth.vue
+++ src/components/common/TurtleAuth.vue
@@ -5,9 +5,15 @@
 <script>
 import auth from '@/auth';
 import firebaseui from 'firebaseui'
+import firebase from 'firebase'
 
 export default {
   name: 'auth',
+  beforeMount(){
+      if(firebase.auth().currentUser){
+        this.$router.push('/');
+      }
+  },
   mounted() {
     auth.initAuthUI('#firebaseui-auth-container');
   },
