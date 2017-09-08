366c0723e90d31dbb52fe34c03a4248137b4843b
woosungchu <woosungchu@gmail.com>
2017-09-08 18:01:34 +0900

    add markdown editor

---
+++ src/components/class/ClassEditor.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="class-editor">
+  <textarea :value="input" @input="update"></textarea>
+  <div v-html="compiledMarkdown"></div>
+</div>
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  name: 'class-editor',
+  data: function(){
+  	return {
+	  input: '# hello'
+	};
+  },
+
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: function (e) {
+      this.input = e.target.value
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+#class-editor {
+  margin: 0;
+  height: 100%;
+  color: #333;
+  /*font-family: 'Helvetica Neue', Arial, sans-serif;*/
+}
+
+textarea, #class-editor div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
+</style>
--- src/components/class/ClassNew.vue
+++ src/components/class/ClassNew.vue
@@ -1,25 +1,24 @@
 <template>
-<div>
-  <div class="column is-8 is-offset-2">
-  	<class-head></class-head>
+<div class="column is-8 is-offset-2">
+  <div class="container">
+	<class-head></class-head>
   </div>
-  <div class="column is-8 is-offset-2">
-    <div class="columns">
-      <div class="column is-8 is-offset-2">
-      	8+2
-      </div>
-    </div>
+  
+  <div class="class-editor-container">
+  	<class-editor></class-editor>
   </div>
+  
 </div>
 </template>
 
 <script>
 import ClassHead from '@/components/class/ClassHead'
+import ClassEditor from '@/components/class/ClassEditor'
 
 export default {
   name: 'class-new',
   components: {
-	  ClassHead
+	  ClassHead,ClassEditor
   },
 
   data () {
@@ -32,5 +31,9 @@ export default {
 </script>
 
 <style scoped>
-
+.class-editor-container{
+  margin : 30px 0px;
+  border: 1px solid #ddd;
+  min-height: 500px;
+}
 </style>
