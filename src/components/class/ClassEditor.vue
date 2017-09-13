3a3cbce0a58c5979f195d61847967e19b128c027
woosungchu <woosungchu@gmail.com>
2017-09-13 18:06:04 +0900

    add title onto editor

--- src/components/class/ClassEditor.vue
+++ src/components/class/ClassEditor.vue
@@ -1,6 +1,16 @@
 <template>
 <div id="class-editor">
-  <turtle-markdown-editor v-bind:name="name" v-bind:value="contents"></turtle-markdown-editor>
+
+  <div id="class-topic">
+	<ul class="list-style-none d-inline">
+	   	<li class="tag float-left">
+	   		<span>vue</span><button>X</button>
+	   		<input id="class_topics[]" class="" type="hidden" autocomplete="off" autofocus="">
+		</li>
+	</ul>
+   	<input id="class-topic-input" class="" type="text" autocomplete="off" autofocus="" placeholder="국어 언어유창성 의사소통">
+  </div>
+  <turtle-markdown-editor v-bind:name="editorName" v-bind:value="editorContents"></turtle-markdown-editor>
 </div>
 </template>
 
@@ -14,8 +24,8 @@ export default {
   },
   data () {
     return {
-      name: "contents",
-      contents : ""
+      editorName: "contents",
+      editorContents : ""
     }
   }
 
@@ -23,4 +33,35 @@ export default {
 </script>
 
 <style scoped>
+#class-topic{
+	display:block;
+    min-height: 34px;
+    padding: 6px 8px;
+    font-size: 14px;
+    line-height: 20px;
+    color: #24292e;
+    vertical-align: middle;
+    background-color: #fff;
+    background-repeat: no-repeat;
+    background-position: right 8px center;
+    border: 1px solid #d1d5da;
+    border-radius: 3px;
+    outline: none;
+    box-shadow: inset 0 1px 2px rgba(27,31,35,0.075);
+}
+
+#class-topic-input{
+	display: inline-block !important;
+	width: 130px;
+    min-height: 26px;
+    max-width: 100%;
+    margin-right: 5px;
+    margin: 0;
+    background: none;
+    box-shadow: none;
+    border: 0 !important;
+    height:20px;
+}
+    
+
 </style>
