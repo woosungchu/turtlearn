4f01043b71f43b44b94e9c2ae9df9889e0f0d3c1
woosungchu <woosungchu@gmail.com>
2017-09-13 16:08:07 +0900

    divide markdown editor

--- src/components/class/ClassEditor.vue
+++ src/components/class/ClassEditor.vue
@@ -1,29 +1,21 @@
 <template>
 <div id="class-editor">
-  <textarea :value="input" @input="update"></textarea>
-  <div v-html="compiledMarkdown"></div>
+  <turtle-markdown-editor v-bind:name="name" v-bind:value="contents"></turtle-markdown-editor>
 </div>
 </template>
 
 <script>
-import marked from 'marked'
+import TurtleMarkdownEditor from '@/components/common/TurtleMarkdownEditor'
 
 export default {
   name: 'class-editor',
-  data: function(){
-  	return {
-	  input: '# hello'
-	};
+  components: {
+	  TurtleMarkdownEditor
   },
-
-  computed: {
-    compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true })
-    }
-  },
-  methods: {
-    update: function (e) {
-      this.input = e.target.value
+  data () {
+    return {
+      name: "contents",
+      contents : ""
     }
   }
 
@@ -31,33 +23,4 @@ export default {
 </script>
 
 <style scoped>
-#class-editor {
-  margin: 0;
-  height: 500px;
-  color: #333;
-}
-
-textarea, #class-editor div {
-  display: inline-block;
-  width: 49%;
-  height: 100%;
-  vertical-align: top;
-  box-sizing: border-box;
-  padding: 20px;
-  overflow-y:scroll;
-}
-
-textarea {
-  border: none;
-  border-right: 1px solid #ccc;
-  resize: none;
-  outline: none;
-  background-color: #f6f6f6;
-  font-size: 14px;
-  font-family: 'Monaco', courier, monospace;
-}
-
-code {
-  color: #f66;
-}
 </style>
---
+++ src/components/common/TurtleMarkdownEditor.vue
@@ -0,0 +1,61 @@
+<template>
+<div id="turtle-markdown-editor">
+  <textarea :name="name" :value="input" @input="update"></textarea>
+  <div v-html="compiledMarkdown"></div>
+</div>
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  name: 'turtle-markdown-editor',
+  props: ['name','value'],
+  data: function(){
+    let defaultValue = '# hello\n## hello2';
+  	return {
+	  input: this.value || defaultValue
+	};
+  },
+
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: function (e) {
+      this.input = e.target.value
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+#turtle-markdown-editor {
+  margin: 0;
+  height: 500px;
+  color: #333;
+}
+
+textarea, #turtle-markdown-editor div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 20px;
+  overflow-y:scroll;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+}
+</style>
